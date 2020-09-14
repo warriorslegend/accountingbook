1f9ff012e15418f315363c95693904e3a0504ca3
warriorslegend <ho_luis10@hotmail.com>
2020-09-14 02:42:10 -0500

    Minor changes

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,14 +4,16 @@ import { Link } from 'react-router-dom';
 
 export default function NavBar() {
     return (
-        <nav className="flex space-between">
+        <nav className="flex space-between align-center">
             <aside>
                 <Link to="/" className="link">
                     <img src="" alt="Project 7"/>
                 </Link>
             </aside>
-            <aside className="flex button">
-                <aside style={{marginRight: '1em'}}>Crea un proyecto</aside>
+            <aside className="flex button align-center">
+                <aside className="styled-button border radius-6px" style={{marginRight: '1em'}}>
+                    Crea un proyecto
+                </aside>
                 <Link to="/account" className="link">
                     <div>Cuenta</div>
                 </Link>
