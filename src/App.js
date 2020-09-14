3922ef0b46344a76b2fe56e2096569167bdf9f83
warriorslegend <ho_luis10@hotmail.com>
2020-09-13 22:04:55 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
+// Libraries
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // Style
 import './css/general.css';
 import './css/App.css';
 // Content
 import NavBar from './components/NavBar/NavBar';
-import HomePath from './paths/HomePath'
+import HomePath from './paths/HomePath';
 
 function App() {
     return (
-        <section>
-            <NavBar/>
-            <HomePath/>
-        </section>
+        <Router>
+            <section>
+                <NavBar/>
+                <Switch>
+                    <Route path="/" exact component={HomePath}/>
+                </Switch>
+            </section>
+        </Router>
     );
 }
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
     return (
         <nav className="flex space-between">
-            <div>Logo</div>
-            <div>Botones</div>
+            <Link to="/" className="link">
+                <div className="button">Logo</div>
+            </Link>
+            <div className="button">Botones</div>
         </nav>
     )
 }
