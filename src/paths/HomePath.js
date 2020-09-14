0829f7bd6655495ad8a822f352759889a87ff779
warriorslegend <ho_luis10@hotmail.com>
2020-09-13 22:25:36 -0500

    Changes

--- src/paths/HomePath.js
+++ src/paths/HomePath.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HomePath() {
     return (
         <article className="flex row">
-            <aside>Option panel</aside>
+            <aside className="option-panel">Option panel</aside>
             <aside>Dashboard</aside>
         </article>
     )
